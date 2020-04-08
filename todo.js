92fade76b68cb0243d02259c3f60d1bc173888bd
ashwini82 <ashwiniatharv1982@gmail.com>
2020-04-07 22:56:48 -0400

    second commit

--- todo.js
+++ todo.js
@@ -1,23 +1,55 @@
+//Adding new list-item
 
-var inputValue = document.getElementById("myInput") ;
-var list = document.getElementById('ul');
-var button = document.getElementById('addbtn');
+$(document).ready(function(){
 
 
-button.addEventListener('click',function(event){
-
-	var ele =document.createElement('li') ;
-	ele.innerHTML = inputValue.value;
-	list.appendChild(ele);
-	inputValue.value ="";
-	
+ var jList = $("#list")
+   
+$('#addbtn').click(function(){
+ 
+ var newEl =$("#myInput").val();
+ jList.append('<li>' +newEl+ '</li>');
+ $("#myInput").val("");
+ deletableItem();
+});
 });
 
-list.addEventListener('click',function(event){
+
+	
+//Removing items from list
+$(document).ready(deletableItem());
+function deletableItem()
+{
+$('li').click(function()
+{
+$(this).addClass('strikeout');
+
+setTimeout(() =>{
+$(this).remove();
+},1000);
+});
+}
+
+// css
+$("div").css({
+   "background-color": "grey",
+    "color" : "black",
+  "padding-left":"30%",
+  "font-size":"20px",
+});
+
+
+
+
+
+
+
+
+/*list.addEventListener('click',function(event){
   event.target.style.textDecoration = "line-through";
 
   setTimeout(function(){
   	  event.target.remove();
   	},2000)
-  })
+  })*/
 
